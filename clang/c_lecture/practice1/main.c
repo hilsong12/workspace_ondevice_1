c000d758df3b8c7a0e726a69c0a656ad087c827f
hilsong12 <hilsong4852@gmail.com>
2025-09-17 19:38:59 +0900

    1

--- clang/c_lecture/practice1/main.c
+++ clang/c_lecture/practice1/main.c
@@ -6,11 +6,11 @@
  */
 int main()
 {
-    int weight ;
+    int weight=0;
     int count=0;
     
 
-    for(weight=80; weight<70; weight--)    //초기값설정후 조건을 본다. for 문장 돈다음에 weight--로 감 그리고 
+    for(weight=80; weight>70; weight--)    //초기값설정후 조건을 본다. for 문장 돈다음에 weight--로 감 그리고 
     {
        printf("운동해서 1kg 빼자\n");
        count++;
