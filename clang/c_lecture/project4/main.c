14931d732bdb36961ac5cfa92c021c1f4234d1c1
hilsong12 <hilsong4852@gmail.com>
2025-09-18 19:42:38 +0900

    0918

--- clang/c_lecture/project4/main.c
+++ clang/c_lecture/project4/main.c
@@ -1,12 +1,254 @@
-#include <stdio.h>
+// #include <stdio.h>
 
 
-/**
- * @brief 배열(array)
- * 
- * @return int 
- */
-int main() 
-{
+// /**
+//  * @brief 배열(array)
+//  * -같은 데이터형의 변수들로 이루어진 유한집합
+//  * -배열의 모든 요소는 연속된 메모리에 저장된다.
+//  * @return int 
+//  */
+// int main() 
+// {
+//     int arr[5];   // 크기가 5개인 배열 선언  
+//     //아직 배열을 초기화 하지 않았음
+//     //배열은 딱 한번만 초기화 가능
+
+
+//     int byteSize=0; //배열의 바이트 크기를 저장할 변수 선언, 초기화
+//     int size=0; // 배열의 크기를 저장할 변수 선언, 초기화
+
+//     byteSize= sizeof(arr);  // 배열의 바이트 크기확인 (반환)
+//     printf("배열의 바이트 크기: %d\n",byteSize);
+
+//     size =sizeof(arr)/sizeof(arr[0]); //배열의 개수?  
+//     printf("배열의 크기:%d\n",size);
+// }
+
+
+// #include<stdio.h>
+// // #define ARR_SIZE   5  // 매크로 상수!!
+
+// int main()
+// {
+//     int arr[ARR_SIZE];  //배열의 크기를 매크로 상수로 지정 가능
+
+//  for(int i=0; i<ARR_SIZE;i++)   //arr에 값을 넣어준다. 0 1 2 3 4
+// {
+//   arr[i] = 0;
+// }
+// printf("arr배열의 원소:");
+// for(int i=0 ; i<ARR_SIZE;i++)   //배열의 값들을 출력한다. 0 0 0 0 0
+// {
+//     printf(" %d",arr[i]);
+// }
+//  printf("\n");
+// }
+
+// int arr[]={0}; //배열의 선언과 
+
+// for ( int i = 0; i < 10; i++)
+// {
+//     printf("arr[%d]는 %d\n",i,arr[i]);
+// }
+// printf ("ARR의 4번째 요소는? :%d \n", arr[3]);
+
+
+// int arr[0] ={0}; // 각 요소 초기화는 중간에 할수 없다.
+// for ( int i = 0; i < 5; i++)
+// {
+//     printf("arr[i]는 %d\n",arr[i]);
+// }
+// printf ("ARR의 4번째 요소는? :%d \n", arr[3]);  
+
+
+// int arr[5];
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4.5;  //4.5 넣으면 int형이여서 짤려서 4로 나옴
+// arr[4]=5;
+// for ( int i = 0; i < 5; i++)
+// {
+//     printf("arr[%d]는 %d\n",i,arr[i]);
+// }
+// }
+
+// #include<stdio.h>
+
+// int main()
+// {
+
+//     int arr[10];  //arr에 값넣기
+//     int i=5;
+
+//     arr[i]=3; // arr[5]-> 6번째
+//     arr[i+1]=4; //arr[6]  ->7번째
+//     arr[i+3] =5; //arr[8]-> 9번째
+
+//     printf("arr의 6번째 요소: %d\n", arr[i]);
+//     printf("arr의 7번째 요소: %d\n", arr[i+1]);
+//      printf("arr의 9번째 요소: %d\n", arr[i+3]);
+// }
+
+
+// #include<stdio.h>
+// #define ARR_SIZE   5  // 매크로 상수!!
+
+// int add(int a ,int b)  // add라는 사용자지정함수 만들기
+// {
+//     return a+b;
+// }
+
+
+// int main()
+// {
+
+//     int arr[ARR_SIZE]={0};   //  전역함수로 지정했기에 가능 ,array 0으로 초기화
+
+//     arr[0]=5;          //arr값 넣어줌
+//     arr[1]=arr[0]+10;
+//     arr[2]=add(arr[0],arr[1]);
+
+//     printf("정수값 2개를 입력하세요\n"); //  0 12 는 지정됐고 34 는 지정해준다
+//     scanf("%d %d",&arr[3],&arr[4]);   //scanf는 주소값에다가 값을 넣는거기 때문에 주소값 가져오기&
+
+//     for (int i = 0; i < ARR_SIZE; i++)
+//     {
+//         printf("%d ",arr[i]);
+//     }
+//     printf("\n");
+//     printf("arr[5]=%d\n",arr[5]); //5값이 없어서 이상한값이 나옴
+
+// }
+
+
+
+
+// #include<stdio.h>
+
+
+// int main()
+// {
+//     char c='A';
+//     char d='B';
+//     int i=5 ,j=6;
+
+
+// printf ("변수 c의 주소값 :%p\t변수 c의 값 :%d\n",&c,c);  //1byte
+// printf ("변수 d의 주소값 :%p\t변수 d의 값 :%d\n",&d,d);  //1byte
+// printf ("변수 i의 주소값 :%p\t변수 i의 값 :%d\n",&i,i);  //4byte
+// printf ("변수 j의 주소값 :%p\t변수 j의 값 :%d\n",&j,j);  //4byte
+
+// }
+
+
+
+// #include<stdio.h>
+
+
+// int main()
+// {
+
+    // int arr[3];
+    // arr[0]=3;
+    // arr[1]=5;
+    // arr[2]=7;
+
+    // printf("arr[0]주소값 :%p\t 요소값%d\n",&arr[0],arr[0]); //cdef
+    // printf("arr[1]주소값 :%p\t 요소값%d\n",&arr[1],arr[1]); //0123
+    // printf("arr[2]주소값 :%p\t 요소값%d\n",&arr[2],arr[2]); //4567
+
+    // int arr[5]={1,2,3};     
+    // for (int i = 0; i < 5; i++)
+    // {
+    //    printf("array[%d] 는 %d\n",i,arr[i]);
+    // }
     
+// }
+
+// }
+#include<stdio.h>
+
+
+int main()
+{
+//     // int arr[3];
+//     // arr[0] = 3;
+//     // arr[1] = 5;
+//     // arr[2] = 7;
+
+//     // printf("arr[0] 주소값 : %p\t요소값 : %d \n", &arr[0], arr[0]);
+//     // printf("arr[1] 주소값 : %p\t요소값 : %d \n", &arr[1], arr[1]);
+//     // printf("arr[2] 주소값 : %p\t요소값 : %d \n", &arr[2], arr[2]);
+
+
+//     // char arr[3];
+//     // arr[0] = 'a';
+//     // arr[1] = 'b';
+//     // arr[2] = 'c';
+
+//     // printf("arr[0] 주소값 : %p\t요소값 : %d \n", &arr[0], arr[0]); // 소문자 a: 아스키코드 97
+//     // printf("arr[1] 주소값 : %p\t요소값 : %d \n", &arr[1], arr[1]); // 소문자 b: 아스키코드 98
+//     // printf("arr[2] 주소값 : %p\t요소값 : %d \n", &arr[2], arr[2]); // 소문자 c: 아스키코드 99
+
+
+// }
+// int decimal;
+//     int binary[20] = { 0, };
+
+//     int position = 0;
+
+//     printf("2진수로 변환할 10진수를 입력하세요 : ");
+//     scanf("%d", &decimal);
+
+//     while (1)
+//     {
+//         binary[position] = decimal % 2;    // 2로 나누었을 때 나머지를 배열에 저장
+//         decimal = decimal / 2;             // 2로 나눈 몫을 저장
+
+//         position++;    // 자릿수 변경
+
+//         if (decimal == 0)    // 몫이 0이 되면 반복을 끝냄
+//             break;
+//     }
+
+//     // 배열의 요소를 역순으로 출력
+//     for (int i = position - 1; i >= 0; i--)
+//     {
+//         printf("%d", binary[i]);
+        
+//     }
+
+//     printf("\n");
+
+//2차원 배열
+//행열-중괄호 안에 중괄호 표현
+
+int arr_2[3][3]=   //행의 크기는 생략가능 (열의 크기는 생략불가!!)
+{
+{1,2,3},
+{4,5,6},
+{7,8,9}
+};
+
+// int arr_22[3][3]={1,2,3,4,5,6,7,8,9};
+
+// int arr_23[3][3]={0}; //전체를 초기화함
+
+
+for (int  i = 0; i < 3; i++)
+{
+    for (int j = 0; j <3 ; j++)
+    {
+        printf("%d행 %d열: %d | 주소: %p",i,j,arr_2[i][j],&arr_2[i][j]);
+        if(j<3)
+         printf("  |  ");
+        if(j==2)
+         printf("\n");        
+
+    }
+    
+}
+
+
 }
---
+++ clang/c_lecture/project5/main.c
@@ -0,0 +1,192 @@
+
+//대망의 포인터
+//포인터란
+//메모리의 주소값을 저장하는 변수임
+//포인터 변수라고도 함
+//포인터는 주소값만 저장할 수 있음    포인터도 변수인데 주소값만 가질수 있다.
+//메모리상에 위치한 특정한 데이터의 시작주소값을 보관한 변수
+// 포인터와 연관된 연산자
+//-> &(주소연산자) :변수의 이름앞에 사용, 해당 변수의 주소값을 반환
+//-> *(참조연산자) : 포인터의 이름이나 주소앞에 사용
+//                   ->포인터가 가리키는 (곳)주소에 저장된 값을 반환한다.
+//-포인터의 크기는 일정하다.
+//-> 플랫폼에 따라서...
+//-> 32bit ->4byte | 64bit-> 8byte
+
+// #include <stdio.h>
+
+// int main() 
+// {
+    
+// }
+
+
+
+#include <stdio.h>
+
+int main() 
+{
+
+    // int *pi;
+    // double *pd;
+    // char *pc;
+
+    // printf("int형의 포인터의 크기: %d\n,",sizeof(pi));
+    // printf("int형의 포인터의 크기: %d\n,",sizeof(pd));
+    // printf("int형의 포인터의 크기: %d\n,",sizeof(pc));
+
+
+
+    // int *ptr; //int 타입의 포인터 선언   int* ptr; 
+    // int *p =NULL;
+
+// int *p;
+// int a;
+// p=&a;
+
+// printf("포인터 변수 p에 들어있는 값: %p\n",p);
+// printf("변수 a의 주소는 ? %p\n",&a);
+// printf("변수 p의 주소는 ? %p\n",&p);
+
+// int *p;
+// int a;
+// p=&a;
+
+// *p=3; //빈방이였는데 넣어졌다.
+
+// printf("a의 값: %d\n",a);
+// printf("p의 값  %d\n",*p);
+
+
+// int a;
+// int b;
+// int *ptr; //포인터 변수 만듬
+
+// ptr=&a;   //ptr도 변수다   
+// *ptr =2;   
+// ptr=&b;
+// *ptr= 3;
+// printf("a의 값: %d\n",a);  //
+//  printf("b의 값 : %d\n",b);
+
+//포인터의 덧셈
+// int a;
+// int *pa;
+// pa=&a;
+
+// int a_1;
+// int *pa_1;
+// pa_1= &a_1;
+
+// *pa=3;
+// pa_1=pa;  //대입 가능
+
+// printf("pa의 주소: %p\n",pa);
+// printf("pa_1의 주소: %p\n",pa_1);   
+
+// char b;
+// char *pb;
+// pb= &b;
+
+// printf("pb의 값: %p\n",pb);
+// printf("pb+1의 값: %p\n",pb+1);         //1byte
+
+// double c;
+// double *pc;
+// pc=&c;
+
+// printf("pc의 값: %p\n",pc);
+// printf("pc+1의 값: %p\n",pc+1);              //8byte
+
+//pc=pa+pb //절대 불가
+//대입은 가능
+
+
+// int arr[10]={1,2,3,4,5,6,7,8,9,10};
+
+// for (int i = 0; i < 10; i++)
+// {
+//     printf("arr[%d]의 주소값: %p\n", i,&arr[i]);
+// }
+
+
+//포인터로 배열 접근
+//  int arr[10]={1,2,3,4,5,6,7,8,9,10};
+//  int *parr;
+
+//  parr=&arr[0];
+
+//  for (int i= 0; i < 10; i++)
+
+//  {
+//     printf("arr[%d]의 주소값: %p  ",i,&arr[i]);
+//     printf("parr+%d의 값: %p",i,(parr+i));
+ 
+//  if (&arr[i]==(parr+i))
+//  {
+//     printf("일치\n");
+//  }
+
+// else
+// printf("불일치\n");
+
+//  }
+
+// printf("arr[3]=%d\n *(parr+3)= %d\n",arr[3],*(parr+3)); //
+
+
+// // 배열[0]과 주소의 관계-이름
+// int arr[3]={1,2,3};
+// int *parr;
+// parr=arr;
+
+// printf("arr의 %p\n",arr); //배열의 이름을 적으면 주소값을 가지고 있다. 
+// printf("arr의 %p\n",&arr);  //arr의 주소를 나타내고 있다.
+// printf("arr의 %d\n",arr); // arr은 주소값(고정)으로 나오고 값으로는 나오지 않는다.
+
+// for (int i = 0; i < 3; i++)
+// {
+// printf("arr[%d]의 %d\n",i,arr[i]); // arr은 주소값(고정)으로 나오고 값으로는 나오지 않는다.
+// }
+
+// printf("arr[0]의 %p\n",&arr[0]);
+
+// printf("arr %d\n",sizeof(arr)); // 배열 전체 크기 = int 3개 = 12바이트 (32비트에서는 12, 64비트도 12)
+// printf("parr %d\n",sizeof(parr));  //sizeof(parr) → 포인터 크기 (주소 크기) = 보통 8바이트 (64비트 환경), 4바이트 (32비트 환경)
+// // 배열의 이름은 주소값을 가지고 있지만 포인터는 아니다.
+
+
+
+// int arr[10]={100,89,99,67,55,77,99,44,55,66};
+// int *parr=arr;
+// int sum=0;
+
+// while (parr-arr <=9)  //???? arr은 값이다 주소값이 아니다 주소끼리 빼는건 불가능한데  arr++가 안된다.
+// {
+//     printf("%p-%p\n",parr, arr); // 
+//     sum += (*parr);
+//     parr++;           //주소를 하나씩 늘리는게 되는구나 
+    
+// }
+// printf("점수평균 %d\n",sum/10);
+
+
+// int a;
+// int *pa;   //포인터 선언
+// int **ppa; //이중포인터 선언
+
+// pa=&a;
+// ppa=&pa;
+
+// a=3;
+
+// printf("a:%d| *pa: %d |**ppa: %d\n",a,*pa,**ppa);
+// printf("a주소:%p| pa가리키는: %p |**ppa저장값: %p\n",&a,pa,*ppa)
+// printf("pa주소:%p| ppa가리키는: %p |",&pa,ppa);
+  
+
+
+
+}
+
+
